8a6c97c5d36710d103348eedc78cf72b6d853321
Miki Tebeka <miki.tebeka@gmail.com>
2012-12-10 09:09:09 -0800

    More testing

--- strftime_test.go
+++ strftime_test.go
@@ -5,17 +5,52 @@ import (
 	"time"
 )
 
-var testTime = time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+type TestCase struct {
+	format, value string
+}
 
-func TestBasic(t *testing.T) {
-	s, err := Format("%a", testTime)
-	if err != nil {
-		t.Fatalf("go error - %s", err)
-	}
-	if s != "Tue" {
-		t.Fatalf("Bad day for %s, got %s - expected Tue", testTime, s)
-	}
+var testTime = time.Date(2009, time.November, 10, 23, 1, 2, 3, time.UTC)
+var testCases = []*TestCase{
+	&TestCase{"%a", "Tue"},
+	&TestCase{"%A", "Tuesday"},
+	&TestCase{"%b", "Nov"},
+	&TestCase{"%B", "November"},
+	&TestCase{"%c", "Tue, 10 Nov 2009 23:01:02 UTC"},
+	&TestCase{"%d", "10"},
+	&TestCase{"%H", "23"},
+	&TestCase{"%I", "11"},
+	&TestCase{"%j", "314"},
+	&TestCase{"%m", "11"},
+	&TestCase{"%M", "01"},
+	&TestCase{"%p", "PM"},
+	&TestCase{"%S", "02"},
+	&TestCase{"%U", "45"},
+	&TestCase{"%w", "2"},
+	&TestCase{"%W", "45"},
+	&TestCase{"%x", "11/10/09"},
+	&TestCase{"%X", "23:01:02"},
+	&TestCase{"%y", "09"},
+	&TestCase{"%Y", "2009"},
+	&TestCase{"%Z", "UTC"},
 
+	// Escape
+	&TestCase{"%%%Y", "%2009"},
+	// Embedded
+	&TestCase{"/path/%Y/%m/report", "/path/2009/11/report"},
+	//Empty
+	&TestCase{"", ""},
+}
+
+func TestFormats(t *testing.T) {
+	for _, tc := range testCases {
+		value, err := Format(tc.format, testTime)
+		if err != nil {
+			t.Fatalf("error formatting %s - %s", tc.format, err)
+		}
+		if value != tc.value {
+			t.Fatalf("error in %s: got %s instead of %s", tc.format, value, tc.value)
+		}
+	}
 }
 
 func TestUnknown(t *testing.T) {
@@ -23,42 +58,4 @@ func TestUnknown(t *testing.T) {
 	if err == nil {
 		t.Fatalf("managed to expand %g")
 	}
-}
-
-func TestDayOfYear(t *testing.T) {
-	s, err := Format("%j", testTime)
-	if err != nil {
-		t.Fatalf("error expanding %j", err)
-	}
-
-	if s != "314" {
-		t.Fatalf("day of year != 314 (got %s)", s)
-	}
-}
-
-func TestWeekday(t *testing.T) {
-	s, err := Format("%w", testTime)
-	if err != nil {
-		t.Fatalf("error expanding %w", err)
-	}
-
-	if s != "2" {
-		t.Fatalf("day of week != 2 (got %s)", s)
-	}
-}
-
-func checkWeek(format string, t *testing.T) {
-	s, err := Format(format, testTime)
-	if err != nil {
-		t.Fatalf("error expanding %s - %s", format, err)
-	}
-
-	if s != "45" {
-		t.Fatalf("[%s] week num != 45 (got %s)", format, s)
-	}
-}
-
-func TestWeekNum(t *testing.T) {
-	checkWeek("%W", t)
-	checkWeek("%U", t)
 }
