8da505a1f9a4575855503000bc3eba932fe85fcf
Miki Tebeka <miki.tebeka@gmail.com>
2020-04-07 09:24:15 +0300

    some work on tests

--- strftime_test.go
+++ strftime_test.go
@@ -10,46 +10,51 @@ type TestCase struct {
 }
 
 var testTime = time.Date(2009, time.November, 10, 23, 1, 2, 3, time.UTC)
-var testCases = []*TestCase{
-	&TestCase{"%a", "Tue"},
-	&TestCase{"%A", "Tuesday"},
-	&TestCase{"%b", "Nov"},
-	&TestCase{"%B", "November"},
-	&TestCase{"%c", "Tue, 10 Nov 2009 23:01:02 UTC"},
-	&TestCase{"%d", "10"},
-	&TestCase{"%H", "23"},
-	&TestCase{"%I", "11"},
-	&TestCase{"%j", "314"},
-	&TestCase{"%m", "11"},
-	&TestCase{"%M", "01"},
-	&TestCase{"%p", "PM"},
-	&TestCase{"%S", "02"},
-	&TestCase{"%U", "45"},
-	&TestCase{"%w", "2"},
-	&TestCase{"%W", "45"},
-	&TestCase{"%x", "11/10/09"},
-	&TestCase{"%X", "23:01:02"},
-	&TestCase{"%y", "09"},
-	&TestCase{"%Y", "2009"},
-	&TestCase{"%Z", "UTC"},
+var testCases = []struct {
+	format string
+	value  string
+}{
+	{"%a", "Tue"},
+	{"%A", "Tuesday"},
+	{"%b", "Nov"},
+	{"%B", "November"},
+	{"%c", "Tue, 10 Nov 2009 23:01:02 UTC"},
+	{"%d", "10"},
+	{"%H", "23"},
+	{"%I", "11"},
+	{"%j", "314"},
+	{"%m", "11"},
+	{"%M", "01"},
+	{"%p", "PM"},
+	{"%S", "02"},
+	{"%U", "45"},
+	{"%w", "2"},
+	{"%W", "45"},
+	{"%x", "11/10/09"},
+	{"%X", "23:01:02"},
+	{"%y", "09"},
+	{"%Y", "2009"},
+	{"%Z", "UTC"},
 
 	// Escape
-	&TestCase{"%%%Y", "%2009"},
+	{"%%%Y", "%2009"},
 	// Embedded
-	&TestCase{"/path/%Y/%m/report", "/path/2009/11/report"},
+	{"/path/%Y/%m/report", "/path/2009/11/report"},
 	//Empty
-	&TestCase{"", ""},
+	{"", ""},
 }
 
 func TestFormats(t *testing.T) {
 	for _, tc := range testCases {
-		value, err := Format(tc.format, testTime)
-		if err != nil {
-			t.Fatalf("error formatting %s - %s", tc.format, err)
-		}
-		if value != tc.value {
-			t.Fatalf("error in %s: got %s instead of %s", tc.format, value, tc.value)
-		}
+		t.Run(tc.format, func(t *testing.T) {
+			value, err := Format(tc.format, testTime)
+			if err != nil {
+				t.Fatalf("error formatting %s - %s", tc.format, err)
+			}
+			if value != tc.value {
+				t.Fatalf("got %s,  expected %s", value, tc.value)
+			}
+		})
 	}
 }
 
